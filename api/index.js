649a004018658d0edbb18e259b276bda0db5a540
Francisco Santos <fsantos.fs37@gmail.com>
2023-09-15 18:19:46 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
