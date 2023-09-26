6410ef1b11fbf9251497ce778da1645967f3e6c8
izael gomes <izaelgomes127@gmail.com>
2023-09-25 22:27:22 -0300

    chore: docker config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 8080,
-  },
-});
+    watch: {
+      usePolling: true,
+    },
+    host: true, 
+    strictPort: true,
+    port: 5173, 
+  }})
